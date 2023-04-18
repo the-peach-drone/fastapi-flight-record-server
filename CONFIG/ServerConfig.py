b2152c46f0bf2a27e6e90f54e4d63ea158d16d45
WTPENGUIN <ccho2932@naver.com>
2023-04-18 11:39:59 +0900

    add config json file

--- CONFIG/ServerConfig.py
+++ CONFIG/ServerConfig.py
@@ -1,13 +1,19 @@
 from pydantic import BaseSettings
-import os, __main__
+import os, json, __main__
 
 class Settings(BaseSettings):
+    # Main Path
+    MAIN_PATH     = os.path.dirname(os.path.realpath(__main__.__file__))
+
+    # Json config
+    config_Path = os.path.join(MAIN_PATH, "CONFIG", "config.json")
+    config = json.load(open(config_Path))
+
     # DB Setting
-    DB_NAME = 'flightCache.db'
+    DB_NAME = config["db_name"]
     DB_PATH = os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)), "Storage", DB_NAME)
 
     # Path Setting
-    MAIN_PATH     = os.path.dirname(os.path.realpath(__main__.__file__))
     LOG_PATH      = os.path.join(MAIN_PATH,    "LOG")
     LOG_FILENAME  = os.path.join(LOG_PATH,     "FileServer.log")
     STORAGE_PATH  = os.path.join(MAIN_PATH,    "Storage")
@@ -15,4 +21,4 @@ class Settings(BaseSettings):
     CSV_DIR_PATH  = os.path.join(STORAGE_PATH, "CSV")
 
     # POST URL Setting
-    POST_URL = "http://127.0.0.1:11111/upload_json"
+    POST_URL = config["api_url"]
