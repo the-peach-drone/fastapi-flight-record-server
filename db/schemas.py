527a70943a550e935a312b6bd4161c09e77a0427
WTPENGUIN <ccho2932@naver.com>
2023-06-15 10:23:59 +0900

    Add requirement SQLAlchemy

--- db/schemas.py
+++ db/schemas.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 
 class Record(BaseModel):
-    serial: str
-    incomming_time: str
-    fileName: str
+    serial         : str
+    incomming_time : str
+    fileName       : str
 
     class Config:
         orm_mode = True
